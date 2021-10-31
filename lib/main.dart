030846382837111e3432eac742c840e8b3f86100
joshneto <joshua.guarda15@gmail.com>
2021-10-31 19:51:42 +0000

    Password widgets added and some better optimization of the rest of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,11 +8,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'PAP Unnamed Keyboard',
-      home: Home(),
+    return MaterialApp(
+      title: 'PAP Unnamed Project',
+      theme: ThemeData.light().copyWith(
+        canvasColor: Colors.transparent,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.blue,
+          secondary: Colors.blue,
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(),
+      home: const Home(),
     );
   }
 }
@@ -20,13 +30,11 @@ class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _Home();
-  }
+  State<StatefulWidget> createState() => _Home();
 }
 
 class _Home extends State<StatefulWidget> {
-  int _count = 0;
+  //int _count = 0;
 
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
@@ -49,13 +57,51 @@ class _Home extends State<StatefulWidget> {
       style: optionStyle,
     ),
   ];
-//
+
+  final List<Widget> _cardList = [
+    const Card(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 80),
+      ),
+    )
+  ];
+
+  Widget _card() {
+    return Card(
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(children: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.vpn_key,
+            ),
+            tooltip: 'Home',
+            iconSize: 30,
+            onPressed: () => setState(() => _selectedIndex = 0),
+          ),
+        ]),
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(30),
+          top: Radius.circular(30),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PAP Unnamed Keyboard',
-      home: Scaffold(
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.light.copyWith(
+        systemNavigationBarColor: Theme.of(context).bottomAppBarColor,
+      ),
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        extendBody: true,
         appBar: AppBar(
+          elevation: 10,
           title: const Center(
             child: Text('PassWords'),
           ),
@@ -65,71 +111,83 @@ class _Home extends State<StatefulWidget> {
             ),
           ),
         ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  <Widget>[
-              _widgetOptions.elementAt(_selectedIndex),
-              Text('You have pressed the button $_count times.')
-              ]
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: _cardList +
+                  const <Widget>[
+                    Card(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 60),
+                      ),
+                    ),
+                  ],
+            ),
           ),
         ),
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            topRight: Radius.circular(30.0),
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Color(0x0f000000),
+                blurRadius: 10,
+              ),
+            ],
           ),
-          child: BottomAppBar(
-            color: Colors.blue,
-            shape: const CircularNotchedRectangle(),
-            notchMargin: 5,
-            child: SizedBox(
-              height: 60.0,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
-                    icon: const Icon(
-                      Icons.vpn_key,
-                      color: Colors.white,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomAppBar(
+              shape: const CircularNotchedRectangle(),
+              notchMargin: 10,
+              child: SizedBox(
+                height: 60.0,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      icon: const Icon(
+                        Icons.vpn_key,
+                      ),
+                      tooltip: 'Home',
+                      onPressed: () => setState(() => _selectedIndex = 0),
                     ),
-                    tooltip: 'Home',
-                    onPressed: () => setState(() => _selectedIndex = 0),
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.white,
+                    IconButton(
+                      icon: const Icon(
+                        Icons.search_sharp,
+                      ),
+                      tooltip: 'Search',
+                      onPressed: () => setState(() => _selectedIndex = 1),
                     ),
-                    tooltip: 'Search',
-                    onPressed: () => setState(() => _selectedIndex = 1),
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                      semanticLabel: "hey",
+                    IconButton(
+                      icon: const Icon(
+                        Icons.delete,
+                        semanticLabel: "hey",
+                      ),
+                      tooltip: 'Delete',
+                      onPressed: () => setState(() => _selectedIndex = 2),
                     ),
-                    tooltip: 'Delete',
-                    onPressed: () => setState(() => _selectedIndex = 2),
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.settings,
-                      color: Colors.white,
+                    IconButton(
+                      icon: const Icon(
+                        Icons.settings,
+                      ),
+                      tooltip: 'Settings',
+                      onPressed: () => setState(() => _selectedIndex = 3),
                     ),
-                    tooltip: 'Settings',
-                    onPressed: () => setState(() => _selectedIndex = 3),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() => _count++),
+          //onPressed: () => setState(() => _count++),
+          onPressed: () => setState(() => _cardList.add(_card())),
           tooltip: 'Increment Counter',
           child: const Icon(Icons.add),
         ),
